a259820bf42eeab30780ba6f8a68db2d41e5e6b0
amanoperation-star <aman.operation@aman.eg>
2026-09-23 12:51:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // <--- هذه الإضافة هي المسؤولة عن تجميع وتحويل تنسيقات Tailwind والألوان للموقع
+  ],
   base: './', // يجعل كافة مسارات الملفات المترجمة نسبية وتعمل على GitHub Pages بدون مشاكل
 })
