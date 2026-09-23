67db988e9e5a9351a50e70dd744522d565385335
amanoperation-star <aman.operation@aman.eg>
2026-09-23 12:36:24 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // السطر ده هو اللي بيخلي كل ملفات الـ JS والـ CSS تفتح صح على GitHub Pages
+  base: './',
 })
