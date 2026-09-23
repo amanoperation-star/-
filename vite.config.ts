2c94ea7382a685fd65b59064c6cede0287e96b8c
amanoperation-star <aman.operation@aman.eg>
2026-09-23 11:18:10 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 يضمن تحميل جميع مسارات JS و CSS و الصور بشكل صحيح على GitHub Pages
+  base: './', // 👈 يضمن تحميل ملفات CSS و JS من المسار الصحيح
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false,
   },
 });
